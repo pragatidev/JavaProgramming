a59ce133b2b81cd79d95a26e221069d3252dfdf7
pragatidev <pragatikunwer@gmail.com>
2017-02-15 13:46:54 -0800

    more programs

--- hackerRank/src/com/leetcode/ContiguousSubArrayLargestSum.java
+++ hackerRank/src/com/leetcode/ContiguousSubArrayLargestSum.java
@@ -3,8 +3,21 @@ package com.leetcode;
 public class ContiguousSubArrayLargestSum {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int arr[] = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+		System.out.println("Maximum subarray Sum is: " + largestContiguousSum(arr));
 
+	}
+
+	private static int largestContiguousSum(int[] arr) {
+		int maxSum = arr[0];
+		int currentSum = maxSum;
+
+		for (int i = 1; i < arr.length; i++) {
+			currentSum = Math.max(arr[i], currentSum + arr[i]);
+			maxSum = Math.max(currentSum, maxSum);
+		}
+
+		return maxSum;
 	}
 
 }
