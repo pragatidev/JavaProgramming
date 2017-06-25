253e67c791bce45f21ad7578193f045d7f733fa0
pragatidev <pragatikunwer@gmail.com>
2017-06-25 16:53:01 -0700

    Longest Palindrome in a sentence

--- hackerRank/src/com/leetcode/LongestPalindrom.java
+++ hackerRank/src/com/leetcode/LongestPalindrom.java
@@ -14,7 +14,37 @@ public class LongestPalindrom {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str = "arbschdhfsmalayalamjfoisjhfs";
+		System.out.println(longestPalindrome(str));
 
+	}
+
+	private static String longestPalindrome(String str) {
+		if (str == null)
+			return str;
+		if (str.length() <= 1)
+			return str;
+
+		String longest = str.substring(0, 1);
+		for (int i = 0; i < str.length(); i++) {
+			String tmp = helper(str, i, i);
+			if (tmp.length() > longest.length())
+				longest = tmp;
+
+			tmp = helper(str, i, i + 1);
+			if (tmp.length() > longest.length())
+				longest = tmp;
+
+		}
+		return longest;
+	}
+
+	private static String helper(String str, int begin, int end) {
+		while (begin >= 0 && end <= str.length() - 1 && str.charAt(begin) == str.charAt(end)) {
+			begin--;
+			end++;
+		}
+		return str.substring(begin + 1, end);
 	}
 
 }
